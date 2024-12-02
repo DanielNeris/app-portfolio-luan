721af777ba1db5aba46b8b819a188c61237af3fb
Daniel de Souza Neris <danielneris01@gmail.com>
2024-12-02 17:32:42 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // This allows Vite to be accessible on any IP address.
+    port: 3000,        // Specify the port (optional)
+  },
   plugins: [react()],
 })
